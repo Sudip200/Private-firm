44a8d7ed2329bcf8b5df77969a1e3ffba5b16bd6
Sudipto Das <98449874+Sudip200@users.noreply.github.com>
2023-02-23 16:24:28 +0000

    adding first product

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
 import './App.css';
-
+const data=[{
+  name:"Sorting & Searching Cheatsheet",
+  category:"Computer Science",
+  link:"https://drive.google.com/file/d/1vsYE6GK7Qg6yomc-TtECSoiIr4D48siE/view?usp=sharing"
+}]
 function App() {
-  const date=new Date()
   return (
     <div className="App">
-      <h1>Welcome to Das Solutions</h1>
-       <h2>{String(date)}</h2>
-       <h3>We are coming soon with lots of exciting projects</h3>
-       <div>
-        <h3>APPOINT MAKER</h3>
+      <h1>CheatSheet Baba</h1>
+       <h3>CheatSheets and notes available currently</h3>
+       <div className='cheatsheet-container'>
+        {data.map((item)=>{
+          return(<div className='cheat-sheet'>
+           <h3>{item.name}</h3>
+           <a href={item.link} >Download</a>
+           <p>{item.category}</p>
+          </div>)
+        })}
        </div>
     </div>
     
