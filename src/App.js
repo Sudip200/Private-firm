16954519475ef3451cd39e8bcda8af3b5ccfe296
Sudipto Das <98449874+Sudip200@users.noreply.github.com>
2023-02-24 04:56:42 +0000

    adding new file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 const data=[{
   name:"Sorting & Searching Cheatsheet",
   category:"Computer Science",
-  link:"https://drive.google.com/file/d/1vsYE6GK7Qg6yomc-TtECSoiIr4D48siE/view?usp=sharing"
+  link:"https://drive.google.com/file/d/1vsYE6GK7Qg6yomc-TtECSoiIr4D48siE/view?usp=sharing",
+  price:"Free",
+  verified:"no"
 },{
   name:"LinkedList Algorithms cheatsheet",
   category:"Computer Science",
-  link:"https://drive.google.com/file/d/15PuktHXd4pAkaaENe07Rug4sPqjeXqJk/view?usp=sharing"
-}]
+  link:"https://drive.google.com/file/d/15PuktHXd4pAkaaENe07Rug4sPqjeXqJk/view?usp=sharing",
+  price:"Free",
+  verified:"no"
+},
+{name:"Stack & Queue Algorithms",
+  category:"Computer Science",
+  link:"https://drive.google.com/file/d/1KXly6pEwRDB6yb_r6uvia6rA2eEU5Dae/view?usp=sharing",
+  price:"Free",
+  verified:"no"}]
 function App() {
   return (
     <div className="App">
@@ -19,7 +28,9 @@ function App() {
           return(<div className='cheat-sheet'>
            <h3>{item.name}</h3>
            <a href={item.link} >Download</a>
+          <h4>Price:{item.price}</h4>
            <p>{item.category}</p>
+           {item.verified==="yes"?<p style={{color:'green'}}>verified</p>:<p style={{color:'red'}}>Not verified</p>}
           </div>)
         })}
        </div>
